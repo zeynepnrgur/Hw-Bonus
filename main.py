83dbdd487d9103e27476fd558cee6519a5208fd4
zeynepnrgur <zeynepgur1503@gmail.com>
2023-11-04 01:01:25 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,8 @@
+
+def kredileriListele():
+    krediler =["Hızlı Kredi", "Maaşını Halkbank'tan alanlara özel", "Mutlu emekli ihtiyaç kredisi"]
+    for kredi in krediler:
+        print("<option>"+kredi+"<option>")
+
+kredileriListele()
+kredileriListele()
---
+++ sartBloklari.py
@@ -0,0 +1,14 @@
+dolarDun = 7.65
+dolarBugun = 7.65
+
+if dolarDun > dolarBugun:
+    print("Azalış Oku")
+    print("Bitti")
+
+elif dolarDun < dolarBugun:
+    print("Artış Oku")
+
+else:
+    print("Eşittir Oku")
+
+    print("Bitti")
